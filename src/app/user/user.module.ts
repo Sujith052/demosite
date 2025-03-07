b689aa79ffac903f335d4da76258437590ec9451
unknown <Sujith052>
2025-03-07 09:59:27 +0530

    user side template setups and routings

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
-
 @NgModule({
   declarations: [
+    UserComponent,
     HeaderComponent,
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule
   ]
 })
-export class HomeModule { }
+export class UserModule { }
